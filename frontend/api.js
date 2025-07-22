837c3ed42928bc97f339b96ff8cb8eb7f8d22a2a
Arjun Anoop <68767302+arjunanoop101@users.noreply.github.com>
2025-07-22 15:33:58 +0530

    Update api.js

--- frontend/api.js
+++ frontend/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://finance-tracker-backend-4eoh.onrender.com",
+  baseURL: "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((req) => {
